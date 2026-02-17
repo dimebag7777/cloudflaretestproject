225c1b2ef0940643a983d7fd166ba7ea147a3318
oscar7777dimebag <oscar.renechat@gmail.com>
2026-02-17 11:02:48 -0600

    change vite and dockercompose yaml

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
+    host: '0.0.0.0',
+    strictPort: true,
+
     port: 5173,
     watch: {
       usePolling: true,
